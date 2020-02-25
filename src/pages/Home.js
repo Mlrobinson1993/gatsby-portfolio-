46e9bbd310f49a2133f72768dd01f27c97c31716
Mikey Robinson <Mlrobinson1993@gmail.com>
2020-02-25 15:49:14 +0800

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import Hero from '../components/sections/Hero';
 import WhyUs from '../components/sections/WhyUs';
 import Reviews from '../components/sections/Reviews';
 import Services from '../components/sections/Services';
-import PrevWork from '../components/sections/PrevWork';
+// import PrevWork from '../components/sections/PrevWork';
 import CTABanner from '../components/utilities/CTABanner';
 import RecentPosts from '../components/sections/RecentPosts';
 
@@ -17,11 +17,11 @@ export default function Home() {
 					<Hero />
 					<WhyUs />
 					<Services />
-					<PrevWork />
+					{/* <PrevWork /> */}
 					<Reviews />
 					<CTABanner
-						HeadingText='We can start getting you'
-						Subheading='the results that you deserve'
+						HeadingText='Get the results that you deserve'
+						Subheading='I can help'
 						CTAText='FIND OUT HOW'
 						linkTo='/services/'
 					/>
